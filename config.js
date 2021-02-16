77d6a32d62c40f0ebe504ba517c722332148da49
Kebur Lulseged <kebur.lulseged@gmail.com>
2021-02-16 12:58:39 -0800

    use Facebook OAuth for user login and authentication

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
-  secretKey: "12345-67890-09876-54321",
-  mongoUrl: "mongodb://localhost:27017/nucampsite",
+  'secretKey': "12345-67890-09876-54321",
+  'mongoUrl': "mongodb://localhost:27017/nucampsite",
+  'facebook': {
+    clientId: "5409196962424088",
+    clientSecret: "1c97945b0289abd69a1bc3295c1cfe62",
+  },
 };
